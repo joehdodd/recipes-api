b3bdc866dcab6a4dac5462630ae5bf389b65fcb0
Joe Dodd <joehdodd@gmail.com>
2019-03-07 12:39:41 -0500

    more solid structure, basic user and recipe routing setup, TODO auth

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -1,1 +1,9 @@
+import { Router } from 'express';
 
+const router = Router();
+
+router.get('/', async (req, res) => {
+  return res.status(200).json({ message: 'Ok!' });
+});
+
+export default router;
