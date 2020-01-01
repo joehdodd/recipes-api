e8722f654f858a9836e1986701fd5a7c3c648600
Joe Dodd <joehdodd@gmail.com>
2020-01-01 14:52:12 -0500

    WIP adds put method for updating recipes

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -28,4 +28,26 @@ router.post('/', JWTAuth, async (req, res) => {
   console.log('create recipe', recipe);
   return res.status(200).json({ message: 'Ok!', data: recipe });
 });
+
+router.put('/:recipeId', JWTAuth, async (req, res) => {
+  const { instructions, ingredients } = req.body;
+  console.log('***', Array.isArray(instructions), '***', Array.isArray(ingredients));
+  let ingredientsArray = Array.isArray(ingredients) ? ingredients : ingredients.split(',').map(s => s.trim());
+  let instructionsArray = Array.isArray(instructions) ? instructions : instructions.split(',').map(s => s.trim());
+  req.context.models.Recipe.update(
+    {
+      title: req.body.title,
+      description: req.body.description,
+      userId: req.user.dataValues.id,
+      ingredients: ingredientsArray,
+      instructions: instructionsArray
+    },
+    { returning: true, where: { id: req.params.recipeId } }
+  )
+    .then(function([rowsUpdated, [recipe]]) {
+      return res.status(200).json({ message: 'Ok!', data: recipe });
+    })
+    .catch(err => console.log(err));
+});
+
 export default router;
